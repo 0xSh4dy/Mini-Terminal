87cb036d156bc1374bc18122b9769149f7851174
r4ksh1t0011 <rakshitawasthi17@gmail.com>
2021-07-26 07:42:26 +0530

    New commit

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
 from discord.ext import commands,tasks
 from lists import *
+from discord import File
 from myClasses.randomStuff import *
 from myClasses.apis import *
 import random
 from pprint import pformat
+from PIL import Image
+from urllib.request import urlretrieve
+import os
 # Reading the token for authentication
 token = open('./token.txt','r').read()
 newBot = commands.Bot(command_prefix='.')
@@ -41,7 +45,7 @@ class Commands(commands.Cog):
 # Making alias for a command
 # @newBot.command(aliases=['comm1','comm2'])
 # async def _someCommand()
-    @commands.command
+    @commands.command()
     async def quest(self,ctx,*,question):
         answer = random.choice(questResponses)
         await ctx.send(f'Question: {question}\nAnswer: {answer}')
@@ -69,9 +73,72 @@ class Commands(commands.Cog):
             dat = searchApi.searchApi(ques)
             await ctx.send(dat)
 
+    @commands.command()
+    async def weather(self,ctx,city):
+        city = str(city)
+        city = city.replace(" ","")
+        if city == "":
+            await ctx.send("Please enter the name of the city to check weather data")
+        else:
+            weatherData = Apis().weatherApi(city)
+            await ctx.send(weatherData)
+
+    @commands.command()
+    async def movie(self,ctx,movie):
+        movie = str(movie)
+        movie = movie.replace(" ","")
+        if movie == "":
+            await ctx.send("Please enter the name of the movie")
+        else:
+            movieData = Apis().movieApi(movie)
+            await ctx.send(movieData)
+
+    @commands.command()
+    async def image(self,ctx,image='random'):
+        image = str(image)
+        image = image.replace(" ","")
+        
+        if image == "":
+            await ctx.send("Please enter the name of the image")
+        else:    
+            imageUrl = Apis().imageApi(image)
+            if imageUrl=="No":
+                await ctx.send("Image not found")
+            else:
+                urlretrieve(imageUrl,"image.jpg")
+                with open('image.jpg','rb') as f:
+                    picture = File(f)
+                await ctx.send(file=picture)
+                cwd = os.getcwd()
+                os.remove(cwd+'/'+'image.jpg')         
+
+class MusicCommands(commands.Cog):
+    def __init__(self,newBot):
+        self.newBot = newBot
+    
+    @commands.command(pass_context=True)
+    async def join(self,ctx):
+        try:
+            channel = ctx.message.author.voice.channel
+            await channel.connect()
+            await ctx.send(f"Mini Terminal joined the channel {channel}")
+        except(AttributeError):
+            res = "You need to be connected to a voice channel in order to use this command"
+            await ctx.send(res)
+
+    @commands.command()
+    async def leave(self,ctx):
+        
+        channel = ctx.message.author.voice.channel
+        await ctx.send(ctx.message.author.voice.channel.disconnect())
+        # except(AttributeError):
+        #     res = "You need to be connected to a voice channel in order to use this command"
+        #     await ctx.send(res)
+
 # Running the bot.
 newBot.add_cog(Events(newBot))
 newBot.add_cog(Commands(newBot))
-newBot.run(token);
+newBot.add_cog(MusicCommands(newBot))
+newBot.run(token)
 
 
--- myClasses/apis.py
+++ myClasses/apis.py
@@ -7,7 +7,7 @@ class Apis:
         filter = str(category)
         filter = filter.replace(" ","")
         queryString = {"format":"json","contains":filter}
-        headers = {'x-rapidapi-key': "your api key",
+        headers = {'x-rapidapi-key': "b7d43b6896mshf7112da639a3c2dp145b9ajsn394528a6394f",
                     'x-rapidapi-host': "jokeapi-v2.p.rapidapi.com"}
         response = requests.get(url=url,headers=headers,params=queryString)
         dat = response.text
@@ -22,7 +22,7 @@ class Apis:
         url='https://google-search5.p.rapidapi.com/google-serps/'
         queryString= {"q":query,"page":"1","rank":1,"hl":"en-US","autocorrect":"1"}
         headers = {
-            'x-rapidapi-key': "your api key",
+            'x-rapidapi-key': "b7d43b6896mshf7112da639a3c2dp145b9ajsn394528a6394f",
             'x-rapidapi-host': "google-search5.p.rapidapi.com"
                 }
         response = requests.get(url=url,headers=headers,params=queryString)
@@ -34,4 +34,42 @@ class Apis:
         d = c[0]
         e=d['snippet']
         return e
+    
+    def weatherApi(self,city):
+        key = "7b7e2d38973b2d857874398ea4af4d5c"
+        url =f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}"
+        response = requests.get(url=url)
+        response = response.text
+        response = pformat(json.loads(response))
+        return response
+
+    def movieApi(self,movie):
+        key = "931573fc170c73de9b62e451e44f666b"
+        url = f"https://api.themoviedb.org/3/search/movie?api_key={key}&query={movie}"
+        response = requests.get(url=url).text
+        response = json.loads(response)
+        response = response['results']
+        response = response[0]
+        title = response['original_title']
+        overview = response['overview']
+        popularity = response['popularity']
+        release_date = response['release_date']
+        vote_average = response['vote_average']
+        result = {'title':title,'overview':overview,'popularity':popularity,'release_date':release_date
+                ,'vote_average':vote_average}
+        result = json.dumps(result)
+        return result        
+
+    def imageApi(self,image='random'):
+        client_id = "jc49qWiXIEV3_dp8wcWqyDQGactRlbv2_kvWpLt6zuA"
+        url = f"https://api.unsplash.com/search/photos/?client_id={client_id}&query={image}"
+        response = requests.get(url=url).text
+        response = json.loads(response)
+        try:
+            response = response['results']
+            response = response[0]['urls']
+            response = response['small']
+        except(IndexError):
+            response = "No"
+        return response
         
